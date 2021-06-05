80311488fceed9685189b2eb981c601643edda9e
matijaskorup1991 <smatija1991@gmail.com>
2021-06-05 14:02:31 +0200

    end

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,84 +1,95 @@
 'use strict';
 
-//HELPER FN:
+//AVOIDING GLOBAL VARIABLES WITH {}:
+{
+  //HELPER FN:
+  let $ = (el) => document.querySelector(el);
+  let $$ = (el) => Array.from(document.querySelectorAll(el));
+  let $on = (el, ev, fn) =>
+    Array.isArray(el)
+      ? el.forEach((o) => $on(o, ev, fn))
+      : el.addEventListener(ev, fn);
 
-let $ = (el) => document.querySelector(el);
-let $$ = (el) => Array.from(document.querySelectorAll(el));
-let $on = (el, ev, fn) =>
-  Array.isArray(el)
-    ? el.forEach((o) => $on(o, ev, fn))
-    : el.addEventListener(ev, fn);
+  //QUIZ DATA:
 
-//QUIZ DATA:
+  let data = [
+    {
+      url: 'https://cdn.playbuzz.com/cdn//f063e8fe-ad57-485e-8211-ed2ee0d9a205/4a864049-816a-479e-8736-51740e8b724b.jpg',
+      question: 'Which ocean lies on the east coast of the United States?',
+      choice: ['Eastern', 'Pacific', 'Indian', 'Atlantic'],
+      answer: 'Atlantic',
+    },
+    {
+      url: 'https://cdn.playbuzz.com/cdn//f063e8fe-ad57-485e-8211-ed2ee0d9a205/4d101ba1-9275-4fb5-ba2c-5606e6c0274e.jpg',
+      question: "Which is the world's highest mountain?",
+      choice: ['K2', 'Makalu', 'Mount Everest', 'Kilimanjaro'],
+      answer: 'Mount Everest',
+    },
+    {
+      url: 'https://cdn.playbuzz.com/cdn//f063e8fe-ad57-485e-8211-ed2ee0d9a205/07121a24-b34b-4711-9bfa-5287163e65ce.jpg',
+      question: 'Which of these cities is not in Europe?',
+      choice: ['Prague', 'Moscow', 'Barcelona', 'Reykjavik'],
+      answer: 'Moscow',
+    },
+    {
+      url: 'https://cdn.playbuzz.com/cdn//f063e8fe-ad57-485e-8211-ed2ee0d9a205/467a486b-be3a-4183-90ed-dd6867d5852d.jpg',
+      question: 'True or False: Iceland is covered in ice.',
+      choice: [true, false],
+      answer: false,
+    },
+    {
+      url: 'https://cdn.playbuzz.com/cdn//f063e8fe-ad57-485e-8211-ed2ee0d9a205/ecf8af7b-8541-4572-b63b-ee7d7f9fc4cc.jpg',
+      question: 'The United Kingdom is comprised of how many countries?',
+      choice: [1, 2, 3, 4],
+      answer: 4,
+    },
+    {
+      url: 'https://cdn.playbuzz.com/cdn//f063e8fe-ad57-485e-8211-ed2ee0d9a205/ecf8af7b-8541-4572-b63b-ee7d7f9fc4cc.jpg',
+      question: 'Which of the following countries do not border France?',
+      choice: ['Germany', 'Netherlands', 'Spain', 'Italy'],
+      answer: 'Netherlands',
+    },
+    {
+      url: 'https://cdn.playbuzz.com/cdn//f063e8fe-ad57-485e-8211-ed2ee0d9a205/6e99b817-7be7-4f8a-9146-3f602ac81fad.jpg',
+      question: 'Which U.S. state is the Grand Canyon located in?',
+      choice: ['Wyoming', 'Arizona', 'New Mexico', 'Nevada'],
+      answer: 'Arizona',
+    },
+    {
+      url: 'https://cdn.playbuzz.com/cdn//f063e8fe-ad57-485e-8211-ed2ee0d9a205/866f119d-e5e2-45ca-846c-b6d10a59d1e4.jpg',
+      question: 'Which is the smallest country, measured by total land area?',
+      choice: ['Maldives', 'Monaco', 'Vatican'],
+      answer: 'Vatican',
+    },
+    {
+      url: 'https://cdn.playbuzz.com/cdn//f063e8fe-ad57-485e-8211-ed2ee0d9a205/13efaf72-d695-4f65-b043-2b805b6a88eb.jpg',
+      question: 'Which is the longest river in the world?',
+      choice: ['Amazon River', 'Congo River', 'Yellow River', 'Nile River'],
+      answer: 'Nile River',
+    },
+    {
+      url: 'https://cdn.playbuzz.com/cdn//f063e8fe-ad57-485e-8211-ed2ee0d9a205/1226f177-dc1a-4142-8875-bdaa177717d7.jpg',
+      question: 'Which is the largest body of water?',
+      choice: ['indian Ocean', 'Pacific Ocean', 'Atlantic Ocean', 'Nile River'],
+      answer: 'Pacific Ocean',
+    },
+  ];
 
-let data = [
-  {
-    url: 'https://cdn.playbuzz.com/cdn//f063e8fe-ad57-485e-8211-ed2ee0d9a205/4a864049-816a-479e-8736-51740e8b724b.jpg',
-    question: 'Which ocean lies on the east coast of the United States?',
-    choice: ['Eastern', 'Pacific', 'Indian', 'Atlantic'],
-    answer: 'Atlantic',
-  },
-  {
-    url: 'https://cdn.playbuzz.com/cdn//f063e8fe-ad57-485e-8211-ed2ee0d9a205/4d101ba1-9275-4fb5-ba2c-5606e6c0274e.jpg',
-    question: "Which is the world's highest mountain?",
-    choice: ['K2', 'Makalu', 'Mount Everest', 'Kilimanjaro'],
-    answer: 'Mount Everest',
-  },
-  {
-    url: 'https://cdn.playbuzz.com/cdn//f063e8fe-ad57-485e-8211-ed2ee0d9a205/07121a24-b34b-4711-9bfa-5287163e65ce.jpg',
-    question: 'Which of these cities is not in Europe?',
-    choice: ['Prague', 'Moscow', 'Barcelona', 'Reykjavik'],
-    answer: 'Moscow',
-  },
-  {
-    url: 'https://cdn.playbuzz.com/cdn//f063e8fe-ad57-485e-8211-ed2ee0d9a205/467a486b-be3a-4183-90ed-dd6867d5852d.jpg',
-    question: 'True or False: Iceland is covered in ice.',
-    choice: [true, false],
-    answer: false,
-  },
-  {
-    url: 'https://cdn.playbuzz.com/cdn//f063e8fe-ad57-485e-8211-ed2ee0d9a205/ecf8af7b-8541-4572-b63b-ee7d7f9fc4cc.jpg',
-    question: 'The United Kingdom is comprised of how many countries?',
-    choice: [1, 2, 3, 4],
-    answer: 4,
-  },
-  {
-    url: 'https://cdn.playbuzz.com/cdn//f063e8fe-ad57-485e-8211-ed2ee0d9a205/ecf8af7b-8541-4572-b63b-ee7d7f9fc4cc.jpg',
-    question: 'Which of the following countries do not border France?',
-    choice: ['Germany', 'Netherlands', 'Spain', 'Italy'],
-    answer: 'Netherlands',
-  },
-  {
-    url: 'https://cdn.playbuzz.com/cdn//f063e8fe-ad57-485e-8211-ed2ee0d9a205/6e99b817-7be7-4f8a-9146-3f602ac81fad.jpg',
-    question: 'Which U.S. state is the Grand Canyon located in?',
-    choice: ['Wyoming', 'Arizona', 'New Mexico', 'Nevada'],
-    answer: 'Arizona',
-  },
-  {
-    url: 'https://cdn.playbuzz.com/cdn//f063e8fe-ad57-485e-8211-ed2ee0d9a205/866f119d-e5e2-45ca-846c-b6d10a59d1e4.jpg',
-    question: 'Which is the smallest country, measured by total land area?',
-    choice: ['Maldives', 'Monaco', 'Vatican'],
-    answer: 'Vatican',
-  },
-  {
-    url: 'https://cdn.playbuzz.com/cdn//f063e8fe-ad57-485e-8211-ed2ee0d9a205/13efaf72-d695-4f65-b043-2b805b6a88eb.jpg',
-    question: 'Which is the longest river in the world?',
-    choice: ['Amazon River', 'Congo River', 'Yellow River', 'Nile River'],
-    answer: 'Nile River',
-  },
-  {
-    url: 'https://cdn.playbuzz.com/cdn//f063e8fe-ad57-485e-8211-ed2ee0d9a205/1226f177-dc1a-4142-8875-bdaa177717d7.jpg',
-    question: 'Which is the largest body of water?',
-    choice: ['indian Ocean', 'Pacific Ocean', 'Atlantic Ocean', 'Nile River'],
-    answer: 'Pacific Ocean',
-  },
-];
+  //RENDER HTML:
+  let h1 = document.createElement('h1');
+  let text = document.createTextNode('Super Quiz!');
 
-//RENDER HTML:
+  h1.appendChild(text);
+  $('#content').appendChild(h1);
 
-data.forEach(
-  (el, i) =>
-    ($('#content').innerHTML += `
+  //MAKE THE COPY OF OBJ:
+  function deepCopy(el) {
+    return JSON.parse(JSON.stringify(el));
+  }
+
+  //CREATE HTML CONTENT:
+  function createHtmlTag(el, i) {
+    $('#content').innerHTML += `
   <article>
   <figure>
   <img class="photos" src=${el.url} alt="Trulli">
@@ -90,16 +101,29 @@ data.forEach(
 ${el.choice.map((input) => `<span class=${i}>${input}</span>`).join('')}
  </div>
 <article>
-`)
-);
-
-//FIND ANSWER:
-
-$on($$('span'), 'click', (e) => {
-  let answerIdx = e.target.className.split(' ')[0];
-  if (String(e.target.innerHTML) == String(data[answerIdx].answer)) {
-    e.target.classList.toggle('green');
-  } else {
-    e.target.classList.toggle('red');
+`;
   }
-});
+
+  //RENDER HTML:
+  (function renderHtml(items) {
+    items.forEach((el, i) => createHtmlTag(el, i));
+  })(deepCopy(data));
+
+  // ANSWER TRUE OR FALSE:
+  function addAnimation(e, className, animation) {
+    let { classList, style } = e.target;
+    classList.toggle(className);
+    style.animation = animation;
+  }
+
+  //FIND ANSWER:
+
+  $on($$('span'), 'click', (e) => {
+    let { innerHTML, className } = e.target;
+    let answerIdx = className.split(' ')[0];
+
+    innerHTML == String(data[answerIdx].answer)
+      ? addAnimation(e, 'green', 'true 0.4s linear')
+      : addAnimation(e, 'red', 'false 0.4s linear');
+  });
+}
